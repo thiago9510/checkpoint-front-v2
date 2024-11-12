c6731e5097b60b745c8187d2518a46c58727a52d
thiago9510 <thiagobatistalucas@gmail.com>
2024-11-12 01:51:24 -0300

    implementação de api para consultar pontos e ajustes na estrutura da tabela

--- src/pages/PontoEletronico/PontoEletronico.tsx
+++ src/pages/PontoEletronico/PontoEletronico.tsx
@@ -1,32 +1,50 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, TablePagination, Typography, Box, TextField } from '@mui/material';
 import AppLayout from '../../components/AppLayout/AppLayout';
+import { useLocation } from 'react-router-dom';
 
-// Dados simulados
-const data = [
-  { date: '01/11/2024', jornada: 8, chegada: '08:00', inicioIntervalo: '12:00', fimIntervalo: '13:00', saida: '18:01', atrasos: '0(0:10)', turno: 'Diurno' },
-  { date: '02/11/2024', jornada: 8, chegada: '08:15', inicioIntervalo: '12:10', fimIntervalo: '13:10', saida: '18:10', atrasos: '1(0:15)', turno: 'Diurno' },
-  { date: '03/11/2024', jornada: 8, chegada: '08:05', inicioIntervalo: '12:05', fimIntervalo: '13:05', saida: '18:05', atrasos: '0(0:05)', turno: 'Diurno' },
-  { date: '01/11/2024', jornada: 8, chegada: '08:00', inicioIntervalo: '12:00', fimIntervalo: '13:00', saida: '18:01', atrasos: '0(0:10)', turno: 'Diurno' },
-  { date: '02/11/2024', jornada: 8, chegada: '08:15', inicioIntervalo: '12:10', fimIntervalo: '13:10', saida: '18:10', atrasos: '1(0:15)', turno: 'Diurno' },
-  { date: '03/11/2024', jornada: 8, chegada: '08:05', inicioIntervalo: '12:05', fimIntervalo: '13:05', saida: '18:05', atrasos: '0(0:05)', turno: 'Diurno' },
-  { date: '01/11/2024', jornada: 8, chegada: '08:00', inicioIntervalo: '12:00', fimIntervalo: '13:00', saida: '18:01', atrasos: '0(0:10)', turno: 'Diurno' },
-  { date: '02/11/2024', jornada: 8, chegada: '08:15', inicioIntervalo: '12:10', fimIntervalo: '13:10', saida: '18:10', atrasos: '1(0:15)', turno: 'Diurno' },
-  { date: '03/11/2024', jornada: 8, chegada: '08:05', inicioIntervalo: '12:05', fimIntervalo: '13:05', saida: '18:05', atrasos: '0(0:05)', turno: 'Diurno' },
-  { date: '01/11/2024', jornada: 8, chegada: '08:00', inicioIntervalo: '12:00', fimIntervalo: '13:00', saida: '18:01', atrasos: '0(0:10)', turno: 'Diurno' },
-  { date: '02/11/2024', jornada: 8, chegada: '08:15', inicioIntervalo: '12:10', fimIntervalo: '13:10', saida: '18:10', atrasos: '1(0:15)', turno: 'Diurno' },
-  { date: '03/11/2024', jornada: 8, chegada: '08:05', inicioIntervalo: '12:05', fimIntervalo: '13:05', saida: '18:05', atrasos: '0(0:05)', turno: 'Diurno' },
-  { date: '03/11/2024', jornada: 8, chegada: '08:05', inicioIntervalo: '12:05', fimIntervalo: '13:05', saida: '18:05', atrasos: '0(0:05)', turno: 'Diurno' },
-  { date: '01/11/2024', jornada: 8, chegada: '08:00', inicioIntervalo: '12:00', fimIntervalo: '13:00', saida: '18:01', atrasos: '0(0:10)', turno: 'Diurno' },
-  { date: '02/11/2024', jornada: 8, chegada: '08:15', inicioIntervalo: '12:10', fimIntervalo: '13:10', saida: '18:10', atrasos: '1(0:15)', turno: 'Diurno' },
-  { date: '03/11/2024', jornada: 8, chegada: '08:05', inicioIntervalo: '12:05', fimIntervalo: '13:05', saida: '18:05', atrasos: '0(0:05)', turno: 'Diurno' },
-  // Adicione mais dados conforme necessário
-];
+interface Ponto {
+  data_registro: string;
+  jornada: number;
+  entrada: string;
+  inicio_intervalo: string;
+  fim_intervalo: string;
+  saida: string;
+  banco_horas: string;
+  turno: string;
+}
 
 const PontoEletronico: React.FC = () => {
+  const [data, setData] = useState<Ponto[]>([]);
   const [page, setPage] = useState(0);
   const [rowsPerPage, setRowsPerPage] = useState(5);
-  const [filterText, setFilterText] = useState("")
+  const [filterText, setFilterText] = useState("");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const token = localStorage.getItem('token');
+
+      try {
+        const response = await fetch('http://localhost:3335/api/consultarPonto', {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
+          }
+        });
+        const result = await response.json();
+        const ponto: Ponto[] = result
+        if (!response.ok) {
+          alert(response);
+        } else {          
+          setData(ponto);
+        }
+      } catch (error) {
+        console.error('Erro ao buscar dados:', error);
+      }
+    };
+    fetchData();
+  }, []);
 
   const handleChangePage = (event: unknown, newPage: number) => {
     setPage(newPage);
@@ -38,13 +56,13 @@ const PontoEletronico: React.FC = () => {
   };
 
   const handleFilterChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setFilterText(event.target.value)
-    setPage(0)
-  }
+    setFilterText(event.target.value);
+    setPage(0);
+  };
 
   const filtred = data.filter(ponto =>
     Object.values(ponto).some(value =>
-      String(value).toLocaleLowerCase().includes(filterText.toLowerCase())
+      String(value).toLowerCase().includes(filterText.toLowerCase())
     )
   );
 
@@ -53,25 +71,16 @@ const PontoEletronico: React.FC = () => {
       <Paper sx={{ width: '100%', overflow: 'hidden', boxShadow: 0, borderRadius: 2, margin: 20 }}>
         <Typography variant="h4" align="center" sx={{ marginBottom: 2 }}></Typography>
 
-
-        {/* Search ponto */}
         <Box display="flex" justifyContent="flex-end">
           <TextField
             label="Search Point"
             variant="outlined"
             value={filterText}
             onChange={handleFilterChange}
-
-            sx={{
-              width: "30%",
-              marginRight: 5
-            }}>
-          </TextField>
+            sx={{ width: "30%", marginRight: 5 }}
+          />
         </Box>
 
-
-
-        {/* Paginação */}
         <TablePagination
           rowsPerPageOptions={[5]}
           component="div"
@@ -82,16 +91,17 @@ const PontoEletronico: React.FC = () => {
           onRowsPerPageChange={handleChangeRowsPerPage}
           sx={{ marginTop: 2, marginRight: 6 }}
         />
+
         <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%' }}>
           <TableContainer
             component={Paper}
             sx={{
-              width: '95%', // Controlando a largura da tabela
-              maxHeight: '700px', // Definindo altura fixa para a tabela
-              overflow: 'auto', // Adicionando a rolagem aqui
-              borderRadius: '10px', // Bordas arredondadas
-              boxShadow: 3, // Sombra suave
-              padding: 0, // Remover qualquer padding extra do Paper
+              width: '95%',
+              maxHeight: '700px',
+              overflow: 'auto',
+              borderRadius: '10px',
+              boxShadow: 3,
+              padding: 0,
             }}
           >
             <Table aria-label="ponto eletrônico">
@@ -99,26 +109,24 @@ const PontoEletronico: React.FC = () => {
                 <TableRow>
                   <TableCell>Data</TableCell>
                   <TableCell>Jornada</TableCell>
-                  <TableCell>Chegada</TableCell>
+                  <TableCell>entrada</TableCell>
                   <TableCell>Início Intervalo</TableCell>
                   <TableCell>Fim Intervalo</TableCell>
                   <TableCell>Saída</TableCell>
-                  <TableCell>Atrasos</TableCell>
+                  <TableCell>Banco de Horas</TableCell>
                   <TableCell>Turno</TableCell>
                 </TableRow>
               </TableHead>
-
-              {/* Corpo da tabela com rolagem e altura fixa */}
               <TableBody>
                 {filtred.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row, index) => (
                   <TableRow key={index} sx={{ backgroundColor: index % 2 === 0 ? '#f9f9f9' : 'white' }}>
-                    <TableCell>{row.date}</TableCell>
+                    <TableCell>{row.data_registro}</TableCell>
                     <TableCell>{row.jornada}</TableCell>
-                    <TableCell>{row.chegada}</TableCell>
-                    <TableCell>{row.inicioIntervalo}</TableCell>
-                    <TableCell>{row.fimIntervalo}</TableCell>
+                    <TableCell>{row.entrada}</TableCell>
+                    <TableCell>{row.inicio_intervalo}</TableCell>
+                    <TableCell>{row.fim_intervalo}</TableCell>
                     <TableCell>{row.saida}</TableCell>
-                    <TableCell>{row.atrasos}</TableCell>
+                    <TableCell>{row.banco_horas}</TableCell>
                     <TableCell>{row.turno}</TableCell>
                   </TableRow>
                 ))}
@@ -126,8 +134,8 @@ const PontoEletronico: React.FC = () => {
             </Table>
           </TableContainer>
         </div>
-      </Paper >
-    </AppLayout >
+      </Paper>
+    </AppLayout>
   );
 };
 
